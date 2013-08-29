cc9ab94e0de68edabc682f9e226eeb0be29e3eca
mukadder <hht179@gmail.com>
2013-08-29 10:50:18 -0400

    new

--- MavenWeb/src/main/java/net/viralpatel/maven/HelloWorldServlet.java
+++ MavenWeb/src/main/java/net/viralpatel/maven/HelloWorldServlet.java
@@ -2,25 +2,70 @@ package net.viralpatel.maven;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 
 public class HelloWorldServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1031422249396784970L;
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		
-		resp.setContentType("text/html");
-		
-		PrintWriter out = resp.getWriter();
-		out.print("Hello World from Servlet");
-		out.flush();
-		out.close();
-	}
+	private DataSource dataSource;
+    private Connection connection;
+    private Statement statement;
+     
+    public void init() throws ServletException {
+        try {
+            // Get DataSource
+            Context initContext  = new InitialContext();
+            Context envContext  = (Context)initContext.lookup("java:/comp/env");
+            dataSource = (DataSource)envContext.lookup("jdbc/testdb");
+ 
+             
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    	PrintWriter out = resp.getWriter();
+    	resp.setContentType("text/html");
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello Servlet Get</h1>");
+		out.println("</body>");
+         
+        ResultSet resultSet = null;
+        try {
+            // Get Connection and Statement
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            String query = "SELECT * FROM Award";
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                out.println(resultSet.getString(1) + resultSet.getString(2) + resultSet.getString(3));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try { if(null!=resultSet)resultSet.close();} catch (SQLException e) 
+            {e.printStackTrace();}
+            try { if(null!=statement)statement.close();} catch (SQLException e) 
+            {e.printStackTrace();}
+            try { if(null!=connection)connection.close();} catch (SQLException e) 
+            {e.printStackTrace();}
+        }
+    }
 }
